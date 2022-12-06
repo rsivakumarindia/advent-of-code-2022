f954617f31c42ab151dc4476aa7a6318dadced69
Sivakumar R <sivakumar.r@nagarro.com>
2022-12-06 12:29:05 +0530

    day 6 solution

---
+++ 6/elf_communication_part2.py
@@ -0,0 +1,12 @@
+with open("data/6.txt", 'r', encoding = 'utf-8') as f:
+    input = f.read().splitlines()
+    input = input[0]
+
+    for p in range(14, len(input)):
+        packet = input[p-14:p]
+        if len(set(packet)) == 14:
+            print(packet)
+            print(p)
+            break
+
+    
